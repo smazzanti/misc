b1b5ed1465fc4fa26dfddf8852c204a31da7226e
smazzanti <41154179+smazzanti@users.noreply.github.com>
2022-03-04 00:39:02 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='misc',
+    version='0.0.1',
+    description='Miscellanea',
+    url='https://github.com/smazzanti/misc',
+    author='Samuele Mazzanti',
+    author_email='mazzanti.sam@gmail.com',
+    license='MIT',
+    packages=['misc'],
+    zip_safe=False
+)
