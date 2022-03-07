707c4c314515e6ed5f780cc91961c9dfb5426697
smazzanti <41154179+smazzanti@users.noreply.github.com>
2022-03-07 18:41:49 +0100

    Update bq_table_summary.py

--- misc/bq_table_summary.py
+++ misc/bq_table_summary.py
@@ -1,10 +1,9 @@
-def bq_table_summary(table_name, bq_client, len_topcounts=10):
+def bq_table_summary(table_name, bq_client, len_quantiles=5, len_topcounts=5):
   
   import pandas as pd
   import jinja2
-        
+  
   # column names
-
   if " " in table_name:
       if table_name[0] != "(":
         table_name = f"({table_name})"
@@ -13,31 +12,27 @@ def bq_table_summary(table_name, bq_client, len_topcounts=10):
   else:
       schema = bq_client.get_table(table_name).schema
       field_names = [field.name for field in schema]
-
+      
   # number of rows
-
   query_nrows = f"SELECT COUNT(*) FROM {table_name}"
   output_nrows = pd.Series(bq_client.query(query_nrows).to_dataframe().iloc[0, 0], index=field_names, name="nrows")
-
+  
   # number of distinct values
-
   jinja_query_distinct = """
   {% set COLUMNS = field_names%}
   SELECT
     {% for COLUMN in COLUMNS -%}
-    ROUND(COUNT(DISTINCT {{COLUMN}}) / COUNT(*)) AS {{COLUMN}}{% if not loop.last %},{% endif %}
+    COUNT(DISTINCT {{COLUMN}}) AS {{COLUMN}}{% if not loop.last %},{% endif %}
     {% endfor -%}
   FROM 
     table_name
   """ \
       .replace('field_names', str(field_names)) \
       .replace('table_name', table_name)
-
   output_distinct = bq_client.query(query=jinja2.Template(jinja_query_distinct).render()).to_dataframe().iloc[0, :]
   output_distinct.name = "distinct"
-
+  
   # number of nulls
-
   jinja_query_nulls = """
   {% set COLUMNS = field_names%}
   SELECT
@@ -49,12 +44,36 @@ def bq_table_summary(table_name, bq_client, len_topcounts=10):
   """ \
       .replace('field_names', str(field_names)) \
       .replace('table_name', table_name)
-
   output_null = bq_client.query(query=jinja2.Template(jinja_query_nulls).render()).to_dataframe().iloc[0, :]
   output_null.name = "null"
 
-  # top counts
+  # quantiles
+  jinja_query_quantiles = """
+  {% set COLUMNS = field_names%}
+  SELECT
+    {% for COLUMN in COLUMNS -%}
+    APPROX_QUANTILES({{COLUMN}}, quantiles) AS {{COLUMN}}{% if not loop.last %},{% endif %}
+    {% endfor -%}
+  FROM 
+    table_name
+  """ \
+      .replace('field_names', str(field_names)) \
+      .replace('table_name', table_name) \
+      .replace('quantiles', str(len_quantiles-1))
+  output_quantiles = bq_client.query(query=jinja2.Template(jinja_query_quantiles).render()).to_dataframe().iloc[0, :]
+  output_quantiles.name = "quantiles"
 
+  def tmp(x, index):
+    try:
+      return pd.Series(x, index=index)
+    except:
+      return pd.Series("", index=index)
+
+  index = ["quantile_{:.0%}".format(x) for x in np.linspace(0,1,len_quantiles)]
+  output_quantiles = output_quantiles.apply(lambda x: tmp(x, index))
+  
+  # top counts
+  
   jinja_query_topcount = """
   {% set COLUMNS = field_names%}
   SELECT
@@ -67,29 +86,24 @@ def bq_table_summary(table_name, bq_client, len_topcounts=10):
       .replace('field_names', str(field_names)) \
       .replace('table_name', table_name) \
       .replace('len_topcounts', str(len_topcounts))
-
   output_topcount = bq_client.query(query=jinja2.Template(jinja_query_topcount).render()).to_dataframe().iloc[0, :]
-
+  
   def format_topcount(output_topcount_row):
-
       l = [(el["value"], round(el["count"] / output_nrows.iloc[0], 2)) for el in output_topcount_row]
       fl = [item for sublist in l for item in sublist]
       fl += [""] * (len_topcounts * 2 - len(fl))
-
       i = [(f"value_{i}", f"count_{i}") for i in range(1,len_topcounts+1)]
       fi = [item for sublist in i for item in sublist]
-
       return pd.Series(fl, index=fi)
-
   output_topcount = output_topcount.apply(format_topcount)
-
+  
   # put all statistics together
-
   output_full = pd.concat([
       output_nrows,
       output_distinct,
       output_null,
+      output_quantiles,
       output_topcount
   ], axis = 1)
-
+  
   return output_full
